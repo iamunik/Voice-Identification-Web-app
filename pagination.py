a732d8abdedee6ebf7b060c8315a0ab78542fcdf
Oluwaseun Ogundeko <102489247+iamunik@users.noreply.github.com>
2025-03-25 16:44:10 +0100

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -5,6 +5,6 @@ homepage = st.Page("webpages/homepage.py", title="Welcome", icon=":material/home
 new_user_page = st.Page("webpages/new_users.py", title="New Users", icon=":material/add_circle:")
 verify_page = st.Page("webpages/verify_users.py", title="User Verification", icon=":material/verified_user:")
 
-pg = st.navigation([homepage, new_user_page, verify_page, team_page])
+pg = st.navigation([homepage, new_user_page, verify_page])
 
 pg.run()
